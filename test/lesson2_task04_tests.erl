56983d3cd50ce03369c742d5101d60df9d42081e
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-14 01:29:03 +0300

    Update lesson2_task04_tests.erl

--- test/lesson2_task04_tests.erl
+++ test/lesson2_task04_tests.erl
@@ -2,7 +2,3 @@
 -import(lesson2_task04, [len/1]).
 -include_lib("eunit/include/eunit.hrl").
 
-len_tests() ->
-    ?assertEqual(3, lesson2_task04:len([a, b, c])),
-    ?assertEqual(5, lesson2_task04:len([1, 2, 3])),
-    ?assertEqual(0, lesson2_task04:len([])).
