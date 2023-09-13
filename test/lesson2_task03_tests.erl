454b220f00955d98e2a8a4b56eb0b5580f198664
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-14 01:18:03 +0300

    Update lesson2_task03_tests.erl

--- test/lesson2_task03_tests.erl
+++ test/lesson2_task03_tests.erl
@@ -3,7 +3,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 element_at_tests() ->
-    ?assertEqual(3, lesson2_task03:element_at([1, 2, 3, 4, 5], 3)),
-    ?assertEqual(5, lesson2_task03:element_at([1, 2, 3, 4, 5], 5)),
-    ?assertEqual(undefined, lesson2_task03:element_at([1, 2, 3, 4, 5], 10)),
+    ?assertEqual(3, lesson2_task03:element_at([1, 2, 3], 3)),
+    ?assertEqual(1, lesson2_task03:element_at([1, 2, 3], 1)),
+    ?assertEqual(undefined, lesson2_task03:element_at([1, 2, 3], 7)),
     ?assertEqual(undefined, lesson2_task03:element_at([_,_])).
