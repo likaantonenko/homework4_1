58e4f8921fba7b8032ae19793e61dc12961efa9b
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-14 10:13:45 +0300

    Update lesson2_task03_tests.erl

--- test/lesson2_task03_tests.erl
+++ test/lesson2_task03_tests.erl
@@ -6,4 +6,4 @@ element_at_tests() ->
     ?assertEqual(3, lesson2_task03:element_at([1, 2, 3], 3)),
     ?assertEqual(1, lesson2_task03:element_at([1, 2, 3], 1)),
     ?assertEqual(undefined, lesson2_task03:element_at([1, 2, 3], 7)),
-    ?assertEqual(undefined, lesson2_task03:element_at([_,_])).
+    ?assertEqual(undefined, lesson2_task03:element_at(fgnya, rle)).
