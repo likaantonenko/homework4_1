7dffd2f32428db7cdbdca00487f1175ebd1fe593
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-14 10:16:06 +0300

    Update lesson2_task03.erl

--- src/lesson2_task03.erl
+++ src/lesson2_task03.erl
@@ -1,6 +1,9 @@
 -module(lesson2_task03).
 -export([element_at/2]).
 
-element_at([H|_], 1) -> H;
-element_at([_|T], N) -> element_at(T, N-1);
-element_at(_,_) -> undefined.
+element_at([H|_], 1) -> 
+  H;
+element_at([_|T], N) -> 
+  element_at(T, N-1);
+element_at(_,_) -> 
+  undefined.
