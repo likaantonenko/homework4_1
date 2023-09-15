b82b81d478514a5bf7f6075cd4c26c1681202027
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-15 16:16:31 +0300

    Update lesson2_task06_tests.erl

--- test/lesson2_task06_tests.erl
+++ test/lesson2_task06_tests.erl
@@ -2,11 +2,11 @@
 -import(lesson2_task06, [is_palindrome/1]).
 -include_lib("eunit/include/eunit.hrl").
 
-is_palindrome_test_() ->
-    ?_assertEqual(true, is_palindrome([])),    
-    ?_assertEqual(true, is_palindrome("tenet")),
-    ?_assertEqual(true, is_palindrome([1, 2, 3, 2, 1])),
-    ?_assertEqual(false, is_palindrome([1, 2, 3, 4, 5])),
-    ?_assertNot(is_palindrome([1, 2, 3, 4, 5])).
+is_palindrome_test() ->
+    ?assertEqual(true, is_palindrome([])),    
+    ?assertEqual(true, is_palindrome("tenet")),
+    ?assertEqual(true, is_palindrome([1, 2, 3, 2, 1])),
+    ?assertEqual(false, is_palindrome([1, 2, 3, 4, 5])),
+    ?assertNot(is_palindrome([1, 2, 3, 4, 5])).
     
     
