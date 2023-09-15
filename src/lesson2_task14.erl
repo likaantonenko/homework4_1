beae7cbdc601f46690cc12ad8d17d2831adaf257
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-15 12:22:22 +0300

    Update lesson2_task14.erl

--- src/lesson2_task14.erl
+++ src/lesson2_task14.erl
@@ -1,6 +1,8 @@
 -module(lesson2_task14).
 -export([duplicate/1]).
 
-duplicate([H|T]) -> [H,H|duplicate(T)];
-duplicate([]) -> [].
+duplicate([H|T]) -> 
+  [H,H|duplicate(T)];
+duplicate([]) -> 
+  [].
 
