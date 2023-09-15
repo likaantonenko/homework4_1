d4bb34ec034cc70637e51fa1f83ce6ef659e5d1e
Angelika Antonenko <106235173+likaantonenko@users.noreply.github.com>
2023-09-15 18:26:36 +0300

    Update lesson2_task08_tests.erl

--- test/lesson2_task08_tests.erl
+++ test/lesson2_task08_tests.erl
@@ -3,6 +3,6 @@
 -include_lib("eunit/include/eunit.hrl").
 
 compress_test() ->
-    ?assertEqual([], compress([])),
+    ?assertEqual([[]], compress([])),
     ?assertEqual([1, 2, 3, 4, 5], compress([1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5])),
     ?assertNot(compress([1, 2, 3, 4, 5])).
